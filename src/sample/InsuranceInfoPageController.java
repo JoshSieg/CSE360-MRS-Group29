3cb35cd616ac29e57428007bb9607f0bb566f9d8
Josh Siegel <89876216+JoshSieg@users.noreply.github.com>
2021-11-10 22:50:24 -0700

    Add files via upload

---
+++ src/sample/InsuranceInfoPageController.java
@@ -0,0 +1,50 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class InsuranceInfoPageController {
+    public TextField insuranceProvider;
+    public TextField healthPlanNumber;
+    public TextField memberID;
+    public Button providerButton;
+    public Button planNumberButton;
+    public Button IDButton;
+    public Button backButton;
+    public SceneController sc = new SceneController();
+
+    public void handleProviderButton (ActionEvent event) {
+        if (providerButton.getText().equals("Edit")) {
+            insuranceProvider.setEditable(true);
+            providerButton.setText("Save");
+        } else if (providerButton.getText().equals("Save")) {
+            insuranceProvider.setEditable(false);
+            providerButton.setText("Edit");
+        }
+    }
+
+    public void handlePlanNumberButton (ActionEvent event) {
+        if (planNumberButton.getText().equals("Edit")) {
+            healthPlanNumber.setEditable(true);
+            planNumberButton.setText("Save");
+        } else if (planNumberButton.getText().equals("Save")) {
+            healthPlanNumber.setEditable(false);
+            planNumberButton.setText("Edit");
+        }
+    }
+
+    public void handleIDButton (ActionEvent event) {
+        if (IDButton.getText().equals("Edit")) {
+            memberID.setEditable(true);
+            IDButton.setText("Save");
+        } else if (IDButton.getText().equals("Save")) {
+            memberID.setEditable(false);
+            IDButton.setText("Edit");
+        }
+    }
+
+    public void handleBackButton (ActionEvent event) {
+
+    }
+}
---
+++ src/sample/PharmacyInfoPageController.java
@@ -0,0 +1,50 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class PharmacyInfoPageController {
+    public TextField pharmacyName;
+    public TextField pharmacyAddress;
+    public TextField pharmacyPhone;
+    public Button nameButton;
+    public Button addressButton;
+    public Button phoneButton;
+    public Button backButton;
+    public SceneController sc = new SceneController();
+
+    public void handleNameButton (ActionEvent event) {
+        if (nameButton.getText().equals("Edit")) {
+            pharmacyName.setEditable(true);
+            nameButton.setText("Save");
+        } else if (nameButton.getText().equals("Save")) {
+            pharmacyName.setEditable(false);
+            nameButton.setText("Edit");
+        }
+    }
+
+    public void handleAddressButton (ActionEvent event) {
+        if (addressButton.getText().equals("Edit")) {
+            pharmacyAddress.setEditable(true);
+            addressButton.setText("Save");
+        } else if (addressButton.getText().equals("Save")) {
+            pharmacyAddress.setEditable(false);
+            addressButton.setText("Edit");
+        }
+    }
+
+    public void handlePhoneButton (ActionEvent event) {
+        if (phoneButton.getText().equals("Edit")) {
+            pharmacyPhone.setEditable(true);
+            phoneButton.setText("Save");
+        } else if (phoneButton.getText().equals("Save")) {
+            pharmacyPhone.setEditable(false);
+            phoneButton.setText("Edit");
+        }
+    }
+
+    public void handleBackButton (ActionEvent event) {
+
+    }
+}
