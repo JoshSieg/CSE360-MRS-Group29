6f288e3c960ee76746cf3611851fe62522c23919
Josh Siegel <89876216+JoshSieg@users.noreply.github.com>
2021-11-11 11:54:03 -0700

    Added prescription page controller class

---
+++ src/sample/PrescriptionsPageController.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class PrescriptionsPageController {
+    public Button addNewButton;
+    public Button backButton;
+
+    public void handleBackButton(ActionEvent event) {
+    }
+
+    public void handleAddNewButton(ActionEvent event) {
+    }
+}
