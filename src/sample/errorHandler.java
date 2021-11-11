7ed0aaeb801929479643c1890660842821c54a35
BobLeek <88070591+BobLeek@users.noreply.github.com>
2021-11-11 15:37:15 -0700

    Add files via upload

---
+++ src/sample/errorHandler.java
@@ -0,0 +1,40 @@
+package sample;
+
+public class errorHandler {
+
+    //error for when user enters invalid login information (will be connected with loginPageController)
+    public void invalidLogin(boolean validlogin){
+        if(validlogin == false){
+            system.out.println("invalid login try again!");
+            sc.switchToLoginPage(event); //RETURN TO LOGIN SCREEN
+        }
+    }
+
+    //ACCOUNT CREATION PAGE: error for when users fails to create an account
+    //check for valid password inside account creation class
+    public void accountCreationCheck(string newPassword, string newUsername){
+
+        boolean validAcc;
+
+        if(newPassword.length() < 5){   //checking if password is longer than 5
+            System.out.println("invalid password length");
+             validAcc = true;
+        }else{
+            validAcc = false;
+        }
+
+        if(newUsername.length() < 3){   //checking if username is longer than 3
+            System.out.println("invalid username length");
+            validAcc = true;
+        }else{
+            validAcc = false;
+        }
+
+        if(validAcc == false){
+            System.out.println("Invalid account creation parameters try again!");
+            //RETURN TO ACCOUNT CREATION PAGE
+            //sc.switchToAccountCreation(event); (needs to be made)
+        }
+    }
+
+}   //end of class
