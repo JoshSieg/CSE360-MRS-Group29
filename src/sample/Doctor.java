cf4baeadd988f506e87aba848036adfa00b657df
JoshSieg <jasiege3@asu.edu>
2021-11-11 20:12:06 -0700

    fixed errors

--- src/sample/Doctor.java
+++ src/sample/Doctor.java
@@ -1,39 +1,37 @@
 package sample;
 
 public class Doctor extends User {
-    public Patient patientList[];
+    public Patient[] patientList;
     public Nurse assignedNurse;
     public int sizeArr;
 
-    public void Doctor(String name, Nurse assignedNurse) {
+    public Doctor(String name, Nurse assignedNurse) {
 
     }
 
-    public void setPatientList(Patient patientList[]) {
-        for(int i = 0; i <= patientList[].length; i++) {
-            this.patientList[i] = patientList[i];
-        }
+    public void setPatientList(Patient[] patientList) {
+        if (patientList.length + 1 >= 0) System.arraycopy(patientList, 0, this.patientList, 0, patientList.length + 1);
     }
 
     public void setAssignedNurse(Nurse assignedNur) {
         assignedNurse = assignedNur;
     }
 
-    public void addPatientToList(Patient patient, Patient patientList[]) {
-        int size = getSizeArr(patientList[]);
+    public void addPatientToList(Patient patient, Patient[] patientList) {
+        int size = getSizeArr(patientList);
         this.patientList[size] = patient;
     }
 
-    public int getSizeArr(Patient patientList[]) {
+    public int getSizeArr(Patient[] patientList) {
         int size = 0;
-        while(patientList[size] != NULL) {
+        while(patientList[size] != null) {
             size++;
         }
         return size;
     }
 
-    public Patient getPatientList() {
-        return patientList[];
+    public Patient[] getPatientList() {
+        return patientList;
     }
 
     public String getAssignedNurse() {
