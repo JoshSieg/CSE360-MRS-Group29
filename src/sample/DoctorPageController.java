55b44f6361fb475a4990ec96e709f2ea4b9ae8fb
Josh Siegel <89876216+JoshSieg@users.noreply.github.com>
2021-11-16 12:19:04 -0700

    Update and rename doctorPageController.java to DoctorPageController.java

--- src/sample/DoctorPageController.java
+++ src/sample/DoctorPageController.java
@@ -6,27 +6,22 @@ import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
-public class doctorPageController {
+public class DoctorPageController {
 
-    public TextField dateField;
-
-    public Button scheduleButton;
+    public TextField patientNameField;
     public Button logoutButton; //go to login page (first page)
-    public Button patientSearchButton; //go to patient search page
+    public Button searchPatientButton;
     public Button viewLastVisitButton;
     public Button goPatientPageButton;
 
+
     public SceneController sc = new SceneController();
 
-    public void handleSchedule() {
-        System.out.println("clicked schedule");
-    }
-
-    public void handleLogout(ActionEvent event) throws IOException{
+    public void handleLogoutButton(ActionEvent event) throws IOException{
         sc.switchToLoginPage(event);
     }
 
-    public void handlePatientSearch() {
+    public void handleSearchPatient() {
         System.out.println("clicked patient search");
     }
 
