5ba4f943030a60c6f4d0605c6878c41cc93d16ab
carsonrobertsg2019 <carsonrobertsg2019@gmail.com>
2021-11-17 22:47:10 -0700

    amended patient page to indicate user's name

--- src/sample/PatientPageController.java
+++ src/sample/PatientPageController.java
@@ -1,8 +1,11 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
 
 import java.io.IOException;
 
@@ -12,7 +15,13 @@ public class PatientPageController {
     public Button pharmacyInfoButton;
     public Button logoutButton;
     public Button insuranceInfoButton;
+    public Label patientName;
 
+//
+    @FXML
+    public void initialize() {
+        patientName.setText(UserManager.getCurrentPatient().getName() + "'s info: ");
+    }
     public SceneController sc = new SceneController();
 
     public void handleLogoutButton(ActionEvent event) throws IOException {
